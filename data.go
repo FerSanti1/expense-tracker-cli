1f972e27a03f88a3755ae2f94f900a4cf7b8220a
Fer1Santi <fernandezsantiago378@gmail.com>
2025-08-13 11:40:29 -0300

    generate initial struct

---
+++ data.go
@@ -0,0 +1,10 @@
+package data
+
+// Package data provides structures for handling financial data.
+
+type Expense struct {
+	ID          int     `json:"id"`
+	Description string  `json:"description"`
+	Amount      float64 `json:"amount"`
+	Date        string  `json:"date"`
+}
